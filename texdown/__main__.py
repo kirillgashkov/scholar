133491bdbeb2f0ecd2596b12538c7e8d67b83458
Kirill Gashkov <kirill@gashkov.com>
2022-07-26 01:50:06 +0500

    Fix argument's help message, change types

--- texdown/__main__.py
+++ texdown/__main__.py
@@ -1,11 +1,29 @@
+from pathlib import Path
+
 import click
 
 
 @click.command()
-@click.option("--output", "-o", type=str, help="A PDF file to output to")
-@click.argument("input", type=str, help="A Markdown file to convert")
-def texdown(output: str, input: str) -> None:
-    pass
+@click.option(
+    "--output",
+    "-o",
+    type=click.Path(
+        dir_okay=False,  # The path can't be a directory
+        writable=True,  # The path must be writable
+        path_type=Path,  # Convert the path to pathlib's Path
+    ),
+    help="A PDF file to output to",
+)
+@click.argument(
+    "input",
+    type=click.Path(
+        exists=True,  # The path must exist
+        readable=True,  # The path must be readable
+        path_type=Path,  # Convert the path to pathlib's Path
+    ),
+)
+def texdown(input: Path, output: Path) -> None:
+    """Convert the INPUT Markdown file to a PDF."""
 
 
 if __name__ == "__main__":
