8966cb03ff1fe3e1b19454949d73f4c398c98c87
Kirill Gashkov <kirill@gashkov.com>
2022-07-26 02:03:38 +0500

    Add the 'input' argument and the 'output' option (#4)
    * Add '--output' option and 'input' argument
    
    * Add help messages, simplify types
    
    * Fix argument's help message, change types
    
    * Format code
    
    * Update parameter types and --output's help message
    
    * Add the '-h' option (a shorthand for '--help')

--- texdown/__main__.py
+++ texdown/__main__.py
@@ -1,9 +1,22 @@
+from pathlib import Path
+
 import click
 
 
 @click.command()
-def texdown() -> None:
-    pass
+@click.option(
+    "--output",
+    "-o",
+    type=click.Path(writable=True, path_type=Path),
+    help="An output file or directory",
+)
+@click.help_option("--help", "-h")
+@click.argument(
+    "input",
+    type=click.Path(exists=True, dir_okay=False, readable=True, path_type=Path),
+)
+def texdown(input: Path, output: Path) -> None:
+    """Convert the INPUT Markdown file to a PDF."""
 
 
 if __name__ == "__main__":
