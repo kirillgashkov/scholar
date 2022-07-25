3d3347d881b6ef377fc6c8a4c8cd6c6e14bc5509
Kirill Gashkov <kirill@gashkov.com>
2022-07-26 01:54:04 +0500

    Format code

--- texdown/__main__.py
+++ texdown/__main__.py
@@ -7,20 +7,12 @@ import click
 @click.option(
     "--output",
     "-o",
-    type=click.Path(
-        dir_okay=False,  # The path can't be a directory
-        writable=True,  # The path must be writable
-        path_type=Path,  # Convert the path to pathlib's Path
-    ),
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
     help="A PDF file to output to",
 )
 @click.argument(
     "input",
-    type=click.Path(
-        exists=True,  # The path must exist
-        readable=True,  # The path must be readable
-        path_type=Path,  # Convert the path to pathlib's Path
-    ),
+    type=click.Path(exists=True, readable=True, path_type=Path),
 )
 def texdown(input: Path, output: Path) -> None:
     """Convert the INPUT Markdown file to a PDF."""
