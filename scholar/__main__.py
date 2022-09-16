5eba89ee8d701feb33050cb8336a917b26b83ffa
Kirill Gashkov <kirill@gashkov.com>
2022-09-16 21:05:45 +0300

    Refactor the main command's parameters

--- scholar/__main__.py
+++ scholar/__main__.py
@@ -1,209 +1,41 @@
-import json
-import shutil
-import subprocess
-import sys
-import os.path
-from collections.abc import Iterable
 from pathlib import Path
 
-import click
-
-from scholar.settings import PANDOC_TEMPLATE_FILEPATH
+import typer
 
 
-def make_pandoc_format(
-    base_format: str,
-    enabled_extensions: Iterable[str] | None = None,
-    disabled_extensions: Iterable[str] | None = None,
-) -> str:
-    if enabled_extensions is None:
-        enabled_extensions = []
-    if disabled_extensions is None:
-        disabled_extensions = []
-
-    return (
-        base_format
-        + "".join(f"+{extension}" for extension in enabled_extensions)
-        + "".join(f"-{extension}" for extension in disabled_extensions)
-    )
-
-
-@click.command()
-@click.option(
-    "--output",
-    "-o",
-    type=click.Path(writable=True, path_type=Path),
-    help="An output file or directory",
-)
-@click.option(
-    "--md-to-tex",
-    is_flag=True,
-    help="Convert to LaTeX instead of PDF.",
-)
-@click.option(
-    "--tex-to-pdf",
-    is_flag=True,
-    help="Convert from LaTeX instead of Markdown.",
-)
-@click.help_option("--help", "-h")
-@click.argument(
-    "input",
-    type=click.Path(exists=True, dir_okay=False, readable=True, path_type=Path),
-)
-def scholar(
-    input: Path, output: Path | None, md_to_tex: bool, tex_to_pdf: bool
-) -> None:
-    """Convert the INPUT Markdown file to PDF."""
-
-    if md_to_tex and tex_to_pdf:
-        raise click.UsageError(
-            "Options '--md-to-tex' and '--tex-to-pdf' are mutually exclusive."
-        )
-
-    if output is None:
-        output = Path.cwd()
-
-    subprocess_workdir = Path(".scholar").absolute()
-    subprocess_workdir.mkdir(parents=True, exist_ok=True)
-
-    input_file = input.absolute()
-    generated_latex_file = subprocess_workdir / input.with_suffix(".tex").name
-    generated_pdf_file = subprocess_workdir / input.with_suffix(".pdf").name
-
-    if md_to_tex:
-        should_generate_latex = True
-        should_generate_pdf = False
-
-        pandoc_input_markdown_file = input_file
-    elif tex_to_pdf:
-        should_generate_latex = False
-        should_generate_pdf = True
-
-        latexmk_input_latex_file = input_file
-    else:
-        should_generate_latex = True
-        should_generate_pdf = True
-
-        pandoc_input_markdown_file = input_file
-        latexmk_input_latex_file = generated_latex_file
-
-    if should_generate_latex:
-        pandoc_markdown_extensions = [
-            # Required extensions
-            "header_attributes",
-            "fenced_divs",
-            "bracketed_spans",
-            "fenced_code_blocks",
-            "backtick_code_blocks",
-            "fenced_code_attributes",
-            "table_captions",
-            "grid_tables",
-            "pipe_tables",
-            "inline_code_attributes",
-            "raw_tex",
-            "implicit_figures",
-            "link_attributes",
-            "citations",
-            "yaml_metadata_block",
-            "tex_math_dollars",
-            # Commonmark-inspired extensions
-            "escaped_line_breaks",
-            "space_in_atx_header",
-            "startnum",
-            "all_symbols_escapable",
-            "intraword_underscores",
-            "shortcut_reference_links",
-            # GFM-inspired extensions
-            "task_lists",
-            "strikeout",
-        ]
-        pandoc_input_format = make_pandoc_format(
-            "markdown_strict", enabled_extensions=pandoc_markdown_extensions
-        )
-        pandoc_output_format = make_pandoc_format(
-            "latex",
-            disabled_extensions=["auto_identifiers", "smart"],
-        )
-        pandoc_command = [
-            "pandoc",
-            "--from",
-            pandoc_input_format,
-            "--to",
-            pandoc_output_format,
-            "--standalone",
-            "--shift-heading-level-by",
-            "-1",
-            "--filter",
-            "pandoc-crossref",
-            "--template",
-            str(PANDOC_TEMPLATE_FILEPATH),
-            "--output",
-            str(generated_latex_file),
-            str(pandoc_input_markdown_file),
-        ]
-
-        click.echo(click.style("Running pandoc", fg="yellow", bold=True))
-        click.echo(click.style(json.dumps(pandoc_command, indent=4), bold=True))
-
-        try:
-            subprocess.run(
-                pandoc_command,
-                stdout=sys.stdout,
-                stderr=sys.stderr,
-                cwd=subprocess_workdir,
-                check=True,
-            )
-        except subprocess.CalledProcessError as e:
-            click.echo(click.style("Running pandoc failed", fg="red", bold=True))
-            sys.exit(1)
-
-    if should_generate_pdf:
-        latexmk_command = [
-            "latexmk",
-            "-xelatex",
-            "-bibtex",
-            "-interaction=nonstopmode",
-            "-halt-on-error",
-            "-file-line-error",
-            "-output-directory=" + str(subprocess_workdir),
-            str(latexmk_input_latex_file),
-        ]
-
-        click.echo(click.style("Running latexmk", fg="yellow", bold=True))
-        click.echo(click.style(json.dumps(latexmk_command, indent=4), bold=True))
-
-        try:
-            subprocess.run(
-                latexmk_command,
-                stdout=sys.stdout,
-                stderr=sys.stderr,
-                cwd=subprocess_workdir,
-                check=True,
-            )
-        except subprocess.CalledProcessError as e:
-            click.echo(click.style("Running latexmk failed", fg="red", bold=True))
-            sys.exit(1)
-
-    if md_to_tex:
-        output_file_default_suffix = ".tex"
-    else:
-        output_file_default_suffix = ".pdf"
-
-    if output.is_dir():
-        output_file = (
-            output.absolute() / input.with_suffix(output_file_default_suffix).name
-        )
-    else:
-        output_file = output.absolute()
-
-    try:
-        if md_to_tex:
-            shutil.copy(generated_latex_file, output_file)
-        else:
-            shutil.copy(generated_pdf_file, output_file)
-    except shutil.SameFileError as f:
-        pass
+def main(
+    input_path: Path = typer.Argument(
+        ...,
+        metavar="INPUT",
+        exists=True,
+        dir_okay=False,
+        readable=True,
+        help="The input Markdown file.",
+    ),
+    output_path: Path = typer.Option(
+        Path.cwd(),
+        "--output",
+        "-o",
+        writable=True,
+        help="The output file or directory.",
+        show_default="CWD",
+    ),
+    convert_to_tex: bool = typer.Option(
+        False,
+        "--to-tex",
+        help="Convert to LaTeX instead of PDF.",
+    ),
+    convert_from_tex: bool = typer.Option(
+        False,
+        "--from-tex",
+        help="Convert from LaTeX instead of Markdown.",
+    ),
+):
+    """
+    Convert the INPUT Markdown file to PDF.
+    """
+    ...
 
 
 if __name__ == "__main__":
-    scholar()
+    typer.run(main)
