15842ffea024fc575ad83cb2bea14ce00f6b8644
Kirill Gashkov <kirill@gashkov.com>
2022-09-22 16:15:03 +0300

    Add cache dirs to settings

--- scholar/settings.py
+++ scholar/settings.py
@@ -2,4 +2,13 @@ from pathlib import Path
 
 ROOT = Path(__file__).parent
 
-PANDOC_TEMPLATE_FILEPATH = ROOT / "pandoc_templates" / "scholar.tex"
+
+PANDOC_TEMPLATES_DIR = ROOT / "pandoc_templates"
+
+PANDOC_TEMPLATE_FILE = PANDOC_TEMPLATES_DIR / "scholar.tex"
+
+
+CACHE_DIR = Path.cwd() / ".scholar"
+
+MD_TO_TEX_CACHE_DIR = CACHE_DIR / "md-to-tex-cache"
+TEX_TO_PDF_CACHE_DIR = CACHE_DIR / "tex-to-pdf-cache"
