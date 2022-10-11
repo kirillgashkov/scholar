33c8eb4f2a30cfd739841e2dfed1cc2aa5b77d2b
Kirill Gashkov <kirill@gashkov.com>
2022-10-11 13:16:05 +0300

    Add cell and row builders

--- scholar/pandoc_lua_filters/make_latex_table.lua
+++ scholar/pandoc_lua_filters/make_latex_table.lua
@@ -1,3 +1,36 @@
+local function make_table_cell_as_inlines(cell)
+    local inlines = pandoc.Inlines({})
+
+    if #cell.contents == 1 and cell.contents[1].tag == "Plain" then
+        inlines = cell.contents[1].content -- "Plain.content" is "Inlines"
+    else
+        warn("make_table_cell_as_inlines: cell.contents is not a simple text block")
+        -- TODO: Consider using pandoc.LineBreak as separator
+        inlines = pandoc.utils.blocks_to_inlines(cell.contents)
+    end
+
+    return inlines
+end
+
+
+local function make_table_row_as_block(cells)
+    local inlines = pandoc.Inlines({})
+
+    inlines:extend(make_table_cell_as_inlines(cells[1]))
+    for i = 2, #cells do
+        inlines:insert(pandoc.Space())
+        inlines:insert(pandoc.RawInline("latex", "&"))
+        inlines:insert(pandoc.Space())
+        inlines:extend(make_table_cell_as_inlines(cells[i]))
+    end
+    inlines:insert(pandoc.RawInline("latex", "\\\\"))
+
+    -- TODO: Consider using pandoc.Span with
+    -- pandoc.LineBreak instead of pandoc.Plain
+    return pandoc.Plain(inlines)
+end
+
+
 local function make_table_spec_as_string()
     return "{c c}" -- FIXME: Replace dummy
 end
@@ -20,8 +53,18 @@ end
 local function make_table_body_as_blocks()
     return pandoc.Blocks({
         -- FIXME: Replace dummy
-        pandoc.RawBlock("latex", "a & b \\\\"),
-        pandoc.RawBlock("latex", "c & d \\\\"),
+        make_table_row_as_block({
+            {contents = {pandoc.Plain({pandoc.Str("a")})}},
+            {contents = {pandoc.Plain({pandoc.Str("b")})}}
+        }),
+        make_table_row_as_block({
+            {contents = {pandoc.Plain({pandoc.Str("c")})}},
+            {contents = {pandoc.Plain({pandoc.Str("d")})}}
+        }),
+        make_table_row_as_block({
+            {contents = {pandoc.Plain({pandoc.Str("e")})}},
+            {contents = {pandoc.Plain({pandoc.Str("f")})}}
+        }),
     })
 end
 
