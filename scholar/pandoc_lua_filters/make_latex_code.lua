8795d79fff8145c4d74925e9e2d1b7e4c6ea87f7
Kirill Gashkov <kirill@gashkov.com>
2022-12-24 03:46:42 +0300

    Add 'make_latex_code.lua'

---
+++ scholar/pandoc_lua_filters/make_latex_code.lua
@@ -0,0 +1,70 @@
+--
+--
+-- NOTE: We are trusing 'language' to be valid latex.
+-- NOTE: We are trusing 'identifier' to be valid latex.
+
+
+local function parse_code_classes(
+    classes -- pandoc.List of string
+)
+    local language
+
+    if #classes > 0 then
+        language = classes[1]
+    else
+        language = nil
+    end
+
+    return {
+        language = language
+    }
+end
+
+
+local function latex_to_inline(
+    latex -- string
+)
+    return pandoc.RawInline("latex", latex)
+end
+
+
+local function make_code(
+    code -- pandoc.Code
+)
+    local parsed_classes = parse_code_classes(code.classes)
+
+    local language = parsed_classes.language
+    if parsed_classes.language ~= nil then
+        language = parsed_classes.language
+    else
+        language = "text"
+    end
+
+    -- \mintinline{python}{def greet(name): print(f"Hello, {name}!")}
+
+    local inlines = pandoc.Inlines({})
+
+    inlines:extend(
+        {
+            -- NOTE: We are trusing 'language' to be valid latex.
+            latex_to_inline("\\mintinline{" .. language .. "}{"),
+            latex_to_inline(code.text),
+            latex_to_inline("}"),
+        }
+    )
+
+    return inlines
+end
+
+
+if FORMAT:match("latex") then
+    return {
+        {
+            Code = function (
+                code_el -- pandoc.Code
+            )
+                return make_code(code_el)
+            end,
+        }
+    }
+end
