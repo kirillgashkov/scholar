4d8bfbce27306f0a1bf4e0bcb1fc27f06c20ed43
Kirill Gashkov <kirill@gashkov.com>
2022-12-24 04:36:17 +0300

    Trim just blank lines

--- scholar/pandoc_lua_filters/trim_code_block.lua
+++ scholar/pandoc_lua_filters/trim_code_block.lua
@@ -3,7 +3,7 @@ return {
         CodeBlock = function (
             code_block_el -- pandoc.CodeBlock
         )
-            code_block_el.text = code_block_el.text:gsub("^%s+", ""):gsub("%s+$", "")
+            code_block_el.text = code_block_el.text:gsub("^\n+", ""):gsub("\n+$", "")
             return code_block_el
         end,
     }
