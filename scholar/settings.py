a727c273ec065d511f634ad9f8f5dce777b6e8cc
Kirill Gashkov <kirill@gashkov.com>
2022-12-26 22:04:33 +0300

    Raise more informative errors in settings

--- scholar/settings.py
+++ scholar/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from typing import Any, Callable
 
-from pydantic import BaseModel, BaseSettings, Extra, PrivateAttr
+from pydantic import BaseModel, BaseSettings, Extra, PrivateAttr, ValidationError
 from pydantic.env_settings import SettingsSourceCallable
 from yaml import safe_load
 
@@ -15,7 +15,14 @@ class ParagraphCaptionSettingsGroup(SettingsGroup):
     listing_prefixes: list[str] = [":", "Listing:"]
 
 
+class SettingsSourceLogEntry(BaseModel):
+    source: str
+    settings: dict[str, Any]
+
+
 class Settings(BaseSettings):
+    _settings_source_log: list[SettingsSourceLogEntry] = PrivateAttr()
+
     _cli_settings: dict[str, Any] = PrivateAttr()
     _yaml_front_matter_settings: dict[str, Any] = PrivateAttr()
     _yaml_config_file: Path | None = PrivateAttr()
@@ -33,8 +40,27 @@ class Settings(BaseSettings):
         self._cli_settings = _cli_settings or {}
         self._yaml_front_matter_settings = _yaml_front_matter_settings or {}
         self._yaml_config_file = _yaml_config_file
+        self._settings_source_log = []
 
-        super().__init__(**init_settings)
+        try:
+            super().__init__(**init_settings)
+        except ValidationError as e:
+            loaded_settings_sources = []
+
+            for settings_source_log_entry in self._settings_source_log:
+                if settings_source_log_entry.settings:
+                    loaded_settings_sources.append(settings_source_log_entry.source)
+
+            settings_sources_hint = (
+                "Loaded settings sources: " + ", ".join(loaded_settings_sources)
+                if loaded_settings_sources
+                else "No settings sources loaded"
+            )
+
+            raise InvalidSettingsError(
+                validation_errors_detail=str(e),
+                settings_sources_hint=settings_sources_hint,
+            )
 
     class Config:
         env_prefix = "scholar_"
@@ -47,13 +73,32 @@ class Settings(BaseSettings):
             file_secret_settings: SettingsSourceCallable,
         ) -> tuple[Callable[["Settings"], dict[str, Any]], ...]:
             return (
-                init_settings,
-                cli_settings_source,
-                env_settings,
-                file_secret_settings,
-                yaml_front_matter_settings_source,
-                yaml_config_file_settings_source,
+                _logged_source("init", init_settings),
+                _logged_source("cli", cli_settings_source),
+                _logged_source("env", env_settings),
+                _logged_source("file_secret", file_secret_settings),
+                _logged_source("yaml_front_matter", yaml_front_matter_settings_source),
+                _logged_source("yaml_config_file", yaml_config_file_settings_source),
             )
+
+
+def _logged_source(
+    source_name: str,
+    settings_source: Callable[[Settings], dict[str, Any]],
+) -> Callable[[Settings], dict[str, Any]]:
+    def wrapper(settings: Settings) -> dict[str, Any]:
+        settings_dict = settings_source(settings)
+
+        settings._settings_source_log.append(
+            SettingsSourceLogEntry(
+                source=source_name,
+                settings=settings_dict,
+            )
+        )
+
+        return settings_dict
+
+    return wrapper
 
 
 def cli_settings_source(settings: Settings) -> dict[str, Any]:
@@ -91,3 +136,11 @@ class ConfigFileNotFoundError(Exception):
 class FailedToLoadConfigFileError(Exception):
     def __init__(self, config_file: Path) -> None:
         self.config_file = config_file
+
+
+class InvalidSettingsError(Exception):
+    def __init__(
+        self, validation_errors_detail: str, settings_sources_hint: str
+    ) -> None:
+        self.validation_errors_detail = validation_errors_detail
+        self.settings_sources_hint = settings_sources_hint
