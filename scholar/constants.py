23a6b20a4029e9308bb8de5f60727e5b5e9bfbff
Kirill Gashkov <kirill@gashkov.com>
2022-12-26 23:59:59 +0300

    Process settings from document's metadata

--- scholar/constants.py
+++ scholar/constants.py
@@ -17,6 +17,8 @@ CACHE_DIR = Path.cwd() / ".scholar"
 MD_TO_TEX_CACHE_DIR = CACHE_DIR / "md-to-tex-cache"
 TEX_TO_PDF_CACHE_DIR = CACHE_DIR / "tex-to-pdf-cache"
 
+SCHOLAR_OUTPUT_DIR = MD_TO_TEX_CACHE_DIR / "scholar-output"
+
 PANDOC_OUTPUT_DIR = MD_TO_TEX_CACHE_DIR / "pandoc-output"
 PANDOC_EXTRACTED_RESOURCES_DIR = MD_TO_TEX_CACHE_DIR / "extracted-resources"
 PANDOC_GENERATED_RESOURCES_DIR = MD_TO_TEX_CACHE_DIR / "generated-resources"
