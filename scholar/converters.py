593e935b649aa64b14000cf98cb95e47d5583ea8
Kirill Gashkov <kirill@gashkov.com>
2023-01-02 02:31:21 +0500

    Use 'render-references-in-text'

--- scholar/converters.py
+++ scholar/converters.py
@@ -233,6 +233,10 @@ class MarkdownToLaTeXConverter(Converter):
                 PandocFilterType.LUA,
             ),
             PandocFilter(
+                self.pandoc_lua_filters_dir / "render-references-in-text.lua",
+                PandocFilterType.LUA,
+            ),
+            PandocFilter(
                 self.pandoc_json_filters_dir / "convert_svg_to_pdf.py",
                 PandocFilterType.JSON,
             ),
