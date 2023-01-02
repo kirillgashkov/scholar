20a793043138ec0ddf3142fffea22fbe24f5d06b
Kirill Gashkov <kirill@gashkov.com>
2023-01-03 01:03:04 +0500

    Process only citations

--- scholar/pandoc_lua_filters/render_citations_in_text.lua
+++ scholar/pandoc_lua_filters/render_citations_in_text.lua
@@ -1,85 +1,41 @@
-local scholar_metadata
-
-
-local function load_scholar_metadata(
-    meta -- pandoc.Meta
-)
-    scholar_metadata = meta.scholar
-end
-
-
-local function is_reference(
+local function is_citation(
     link -- pandoc.Link
 )
     return link.content == pandoc.Inlines({pandoc.Str("@")})
 end
 
 
-local function validate_reference(
+local function validate_citation(
     link -- pandoc.Link
 )
     if link.target:sub(1, 1) ~= "#" then
-        error("Reference target must start with '#': " .. link.target)
+        error("Citation target doesn't start with '#': " .. link.target)
     end
 end
 
 
-local function is_reference_citation(
-    link -- pandoc.Link
-)
-    return scholar_metadata.references[link.target:sub(2)] ~= nil
-end
-
-
-local function reference_to_reference_id(
+local function citation_to_citation_id(
     link -- pandoc.Link
 )
     return link.target:sub(2)
 end
 
 
-local function reference_to_latex_cite_inline(
-    link -- pandoc.Link
-)
-    return pandoc.RawInline(
-        "latex",
-        "\\cite{" .. reference_to_reference_id(link) .. "}"
-    )
-end
-
-
-local function reference_to_latex_ref_inline(
-    link -- pandoc.Link
-)
-    return pandoc.RawInline(
-        "latex",
-        "\\ref{" .. reference_to_reference_id(link) .. "}"
-    )
-end
-
-
 return {
-    {
-        Meta = function (meta)
-            load_scholar_metadata(meta)
-            return meta
-        end,
-    },
     {
         Link = function (
             link -- pandoc.Link
         )
-            if not is_reference(link) then
+            if not is_citation(link) then
                 return link
             end
 
-            validate_reference(link)
+            validate_citation(link)
 
-            if is_reference_citation(link) then
-                return reference_to_latex_cite_inline(link)
-            else
-                return reference_to_latex_ref_inline(link)
-            end
+            return pandoc.RawInline(
+                "latex",
+                "\\cite{" .. citation_to_citation_id(link) .. "}"
+            )
         end,
     }
 }
