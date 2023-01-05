f0c138d5b1ed09e149d613d7b080f6f125832fa7
Kirill Gashkov <kirill@gashkov.com>
2023-01-05 14:47:27 +0500

    Add 'render_table_of_contents_container.lua'

---
+++ scholar/pandoc_lua_filters/render_table_of_contents_container.lua
@@ -0,0 +1,35 @@
+local function is_table_of_contents_container(
+    div -- pandoc.Div
+)
+    return div.identifier == "table-of-contents"
+end
+
+
+local function check_table_of_contents_container(
+    div -- pandoc.Div
+)
+    if #div.content > 0 then
+        warn("table_of_contents container isn't empty. Ignoring its content.")
+    end
+end
+
+
+return {
+    {
+        Div = function (
+            div -- pandoc.Div
+        )
+            if not is_table_of_contents_container(div) then
+                return div
+            end
+
+            check_table_of_contents_container(div)
+
+            local rendered = table.concat({
+                "\\emph{Table of contents will be rendered here.}",
+            }, "\n")
+
+            return pandoc.RawBlock("latex", rendered)
+        end,
+    }
+}
