fe196e9c3a6f55bde7658f318f4f8e9df0d618bb
Kirill Gashkov <kirill@gashkov.com>
2023-01-11 19:30:09 +0300

    Implement tocless headers

--- scholar/converters.py
+++ scholar/converters.py
@@ -199,11 +199,12 @@ class MarkdownToLaTeXConverter(Converter):
                 PandocFilterType.LUA,
             ),
             PandocFilter(
-                self.pandoc_lua_filters_dir / "tocless_headers.lua",
+                self.pandoc_lua_filters_dir / "make_and_render_sections.lua",
                 PandocFilterType.LUA,
             ),
+            # NOTE: tocless_headers should run after make_and_render_sections.
             PandocFilter(
-                self.pandoc_lua_filters_dir / "make_and_render_sections.lua",
+                self.pandoc_lua_filters_dir / "tocless_headers.lua",
                 PandocFilterType.LUA,
             ),
             PandocFilter(
