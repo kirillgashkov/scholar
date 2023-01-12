c97e93b9b26fb2d71fadb191b8e0774fb732be24
Kirill Gashkov <kirill@gashkov.com>
2023-01-12 15:42:40 +0300

    Rename 'render_references_in_text'

--- scholar/converters.py
+++ scholar/converters.py
@@ -169,7 +169,7 @@ class MarkdownToLaTeXConverter(Converter):
                 PandocFilterType.LUA,
             ),
             PandocFilter(
-                self.pandoc_lua_filters_dir / "render_references_in_text.lua",
+                self.pandoc_lua_filters_dir / "render_link_reference.lua",
                 PandocFilterType.LUA,
             ),
             PandocFilter(
